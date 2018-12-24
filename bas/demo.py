e8a5c4830f7ad28dd0770a32323207923a6b56f3
xds <sxd95@qq.com>
2018-12-24 14:48:41 +0800

    增加起始点和终点

--- bas/demo.py
+++ bas/demo.py
@@ -18,21 +18,24 @@ import numpy as np
 
 # 求 z = -cos(x) - sin(y)(x in [1, 2*pi], y in [1, 2*pi]) 的最大值
 
-def fun(vars):
+def fun(vars, po_st=18, po_en=18):
     # 评价函数
     # x, y = vars
     # x = np.array(range(0, 11))
     x = np.random.rand(11)
     x[0:11] = vars
-    if 0 <= x.any() <= 10:
+    if all(x >= 0 ) and all(x <= 36):
     # return -np.cos(x) - np.sin(y) + 10
         fitness = 0
+        fitness = math.sqrt(pow(x[0] - po_st, 2) + 1)
+
         for i in range(0, 10):
             fitness += math.sqrt(pow(x[i+1] - x[i], 2) + 1)
-        return fitness
 
+        fitness += math.sqrt(pow(po_en - x[10], 2)+ 1 )
+        return fitness
     else:
-        return 1000  # 返回一个达不到的小/大值
+        return 100  # 返回一个达不到的小/大值
 
 
 bas = RBAS(fitness_function=fun, dim=11, steps=1000, eta=0.997,
