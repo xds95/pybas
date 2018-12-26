e31a1227f32680dce551eb0284623d588de84e46
xds <sxd95@qq.com>
2018-12-26 14:22:18 +0800

    增添输入参数

--- bas/demo.py
+++ bas/demo.py
@@ -18,27 +18,30 @@ import numpy as np
 
 # 求 z = -cos(x) - sin(y)(x in [1, 2*pi], y in [1, 2*pi]) 的最大值
 
-def fun(vars, po_st=18, po_en=18):
+def fun(chromosome, dim=None, *, po_st=None, po_en=None):
     # 评价函数
-    # x, y = vars
-    # x = np.array(range(0, 11))
-    x = np.random.rand(11)
-    x[0:11] = vars
-    if all(x >= 0 ) and all(x <= 36):
-    # return -np.cos(x) - np.sin(y) + 10
-        fitness = 0
+    """
+    :param chromosome: 点的位置
+    :param dim: 数据点的数量
+    :param bound: 边界
+    :param po_st: 起始点
+    :param po_en: 终点
+    :param map_point: x暂时废弃x
+    :return:
+    """
+    x = np.random.rand(dim)
+    x[0:dim] = chromosome
+    if all(x >= 0) and all(x <= 20):
         fitness = math.sqrt(pow(x[0] - po_st, 2) + 1)
-
-        for i in range(0, 10):
+        for i in range(0, dim-1):
             fitness += math.sqrt(pow(x[i+1] - x[i], 2) + 1)
-
-        fitness += math.sqrt(pow(po_en - x[10], 2)+ 1 )
+        fitness += math.sqrt(pow(po_en - x[dim-1], 2) + 1)
         return fitness
     else:
-        return 100  # 返回一个达不到的小/大值
+        return 30  # 返回一个达不到的小/大值
 
 
-bas = RBAS(fitness_function=fun, dim=11, steps=1000, eta=0.997,
-           bound=[0, 10], step0=3, fitness_value=np.inf)
+bas = RBAS(fitness_function=fun, dim=20, steps=1000, eta=0.997, bound=[5, 15],
+           step0=5, fitness_value=np.inf, po_st=10, po_en=10)
 bas.run()
 print(bas.gbest)
--- bas/rbas.py
+++ bas/rbas.py
@@ -17,7 +17,8 @@ npr = np.random
 
 class RBASEngine(BASEngine):
     def __init__(self, steps=100, eps=0.01, chromosome=None, dim=2, bound=None, fitness_function=None, *,
-                 fitness_value=-np.inf, init_method='random', step0=1, c=5, eta=0.95, **kwargs):
+                 fitness_value=-np.inf, init_method='random', step0=1, c=5, eta=0.95, po_st=18, po_en=18,
+                 **kwargs):
         """
         :param steps: 迭代次数
         :param eps: 精度
@@ -33,20 +34,21 @@ class RBASEngine(BASEngine):
         :param kwargs:
         """
         super().__init__(steps, eps, chromosome, dim, bound, fitness_function, fitness_value=fitness_value,
-                         init_method=init_method, step0=step0, c=c, eta=eta, **kwargs)
+                         init_method=init_method, step0=step0, c=c, eta=eta, po_st=po_st, po_en=po_en,
+                         **kwargs)
 
     def update(self, *args, **kwargs):
         d0 = self.step / self.c
         dir = npr.rand(self.dim) - 1
         dir = dir / (self.eps + np.linalg.norm(dir))
         xleft = self.chromosome + dir * d0
-        fleft = self.fitness_function(xleft)
+        fleft = self.fitness_function(xleft, self.dim, po_st=self.po_st, po_en=self.po_en)
         xright = self.chromosome - dir * d0
-        fright = self.fitness_function(xright)
+        fright = self.fitness_function(xright, self.dim, po_st=self.po_st, po_en=self.po_en)
 
         if self.fitness() < self.gbest.fitness_value:
             self.gbest = deepcopy(self)
-            self.chromosome -= self.step * dir * np.sign(-fleft + fright)
+            self.chromosome += self.step * dir * np.sign(-fleft + fright)
         else:
             if self.step * npr.rand() < self.step :
                 self.chromosome -= self.step * dir * np.sign(fleft - fright)
